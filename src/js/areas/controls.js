2aed76cf98a2202dbe3fc61de2fb0a66d298da6b
Hakan Bilgin <hbi99@hotmail.com>
2025-11-03 15:33:15 +0100

    ui/ux

--- src/js/areas/controls.js
+++ src/js/areas/controls.js
@@ -6,17 +6,30 @@
 		// fast references
 		this.els = {
 			content: window.find("content"),
+			controls: window.find(".controls"),
 		};
 	},
 	dispatch(event) {
 		let APP = ozil,
 			Self = APP.controls,
+			value,
 			el;
 		// console.log(event);
 		switch (event.type) {
 			// custom events
 			case "toggle-play":
 				console.log(event);
+				break;
+			case "toggle-menu":
+				el = Self.els.controls.find(`.ctrl-menu.show`);
+				if (!el.isSame(event.el)) {
+					Self.els.controls.find(`span[data-click="toggle-menu"].active`).removeClass("active");
+					el.removeClass("show");
+				}
+
+				value = event.el.hasClass("active");
+				event.el.toggleClass("active", value);
+				Self.els.controls.find(`.ctrl-menu.${event.el.data("arg")}`).toggleClass("show", value);
 				break;
 		}
 	}
--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,6 +1,9 @@
 
 let Test = {
 	init(APP) {
-		
+		// return;
+
+		return APP.content.find(`.controls span[data-click="toggle-menu"]`).get(0).trigger("click");
+
 	}
 };
